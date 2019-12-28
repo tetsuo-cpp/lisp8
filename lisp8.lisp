9ff09e8b649681bd04f29d648a8e3b9db6d98cbb
Alex Cameron <ascottcameron@gmail.com>
2019-12-28 18:52:31 +1100

    Begin reading opcodes.

--- lisp8.lisp
+++ lisp8.lisp
@@ -29,29 +29,60 @@
     #xF0 #x80 #xF0 #x80 #xF0 ;; E
     #xF0 #x80 #xF0 #x80 #x80 ;; F
     ))
+;; There are 16 registers on a CHIP-8 machine.
+(defconstant +register-num+ 16)
+;; Ops are two bytes in length.
+(defconstant +op-size+ 2)
 
 (defclass rom ()
   ((memory
     :initform
     (make-array +max-memory-addr+
-                :element-type 'unsigned-byte
-                :initial-element #x0))
+                :element-type '(unsigned-byte 8)
+                :initial-element #x0)
+    :accessor memory)
    (addr
-    :initform +rom-addr+)
+    :initform +rom-addr+
+    :accessor addr)
    (callstack
     :initform
-    (make-array 0 :fill-pointer 0 :adjustable t))))
+    (make-array 0 :fill-pointer 0 :adjustable t)
+    :accessor callstack)))
 
 (defun load-fonts (rom)
   (dotimes (i (length +font-set+))
-    (setf (aref (slot-value rom 'memory) i) (aref +font-set+ i))))
+    (setf (aref (memory rom) i) (aref +font-set+ i))))
 
 (defun load-rom (filename rom)
-  (with-open-file (file filename :direction :input)
-    (read-sequence (slot-value rom 'memory) file :start (slot-value rom 'addr))))
+  (with-open-file (file filename
+                        :direction :input
+                        :element-type '(unsigned-byte 8))
+    (read-sequence (memory rom) file :start (addr rom))))
+
+(defun read-op (rom)
+  (when (< (addr rom) +max-memory-addr+)
+    (let* ((first-byte (aref (memory rom) (addr rom)))
+           (second-byte (aref (memory rom) (1+ (addr rom))))
+           (op (logior (ash first-byte 8) second-byte)))
+      (incf (addr rom) +op-size+)
+      op)))
+
+(defclass cpu ()
+  ((ins
+    :initform #x0
+    :accessor ins)
+   (registers
+    :initform
+    (make-array +register-num+
+                :element-type 'unsigned-byte
+                :initial-element #x0)
+    :accessor registers)))
 
 (defun run-rom (filename)
-  (let ((rom (make-instance 'rom)))
+  (let ((rom (make-instance 'rom))
+        (cpu (make-instance 'cpu)))
     (load-fonts rom)
     (load-rom filename rom)
-    (format t "text was: ~a~%" (slot-value rom 'memory))))
+    (let ((op))
+      (loop while (setf op (read-op rom)) do
+           (format t "op is: ~a~%" op)))))
